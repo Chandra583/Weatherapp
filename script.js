374956e2ff868ef6819d0b7a85c3b1a01f935fe7
Chandrashekhar gowda KS <106522596+Chandra583@users.noreply.github.com>
2023-01-05 23:49:17 +0530

    new

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
 let weather = {
-    "apiKey":"1c31aadf495a50850b5eebef10ba447d",
-}
+    apiKey: "1c31aadf495a50850b5eebef10ba447d",
+    fetchWeather: function(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=mysore&appid=1c31aadf495a50850b5eebef10ba447d"
+        ).then((response) =>response.json()
+        ).then((data) => console.log(data));
+    },
+};
+// let weadther = {
+//     apiKey: "1c31aadf495a50850b5eebef10ba447d",
+//     fetchWeather: function (city) {
+//       fetch(
+//         "https://api.openweathermap.org/data/2.5/weather?q=" +
+//           city +
+//           "&units=metric&appid=" +
+//           this.apiKey
+//       )
+//         .then((response) => {
+//           if (!response.ok) {
+//             alert("No weather found.");
+//             throw new Error("No weather found.");
+//           }
+//           return response.json();
+//         })
+//         .then((data) => this.displayWeather(data));
+//     },
+//     displayWeather: function (data) {
+//       const { name } = data;
+//       const { icon, description } = data.weather[0];
+//       const { temp, humidity } = data.main;
+//       const { speed } = data.wind;
+//       document.querySelector(".city").innerText = "Weather in " + name;
+//       document.querySelector(".icon").src =
+//         "https://openweathermap.org/img/wn/" + icon + ".png";
+//       document.querySelector(".description").innerText = description;
+//       document.querySelector(".temp").innerText = temp + "°C";
+//       document.querySelector(".humidity").innerText =
+//         "Humidity: " + humidity + "%";
+//       document.querySelector(".wind").innerText =
+//         "Wind speed: " + speed + " km/h";
+//       document.querySelector(".weather").classList.remove("loading");
+//       document.body.style.backgroundImage =
+//         "url('https://source.unsplash.com/1600x900/?" + name + "')";
+//     },
+//     search: function () {
+//       this.fetchWeather(document.querySelector(".search-bar").value);
+//     },
+//   };
+  
+//   document.querySelector(".search button").addEventListener("click", function () {
+//     weather.search();
+//   });
+  
+//   document
+//     .querySelector(".search-bar")
+//     .addEventListener("keyup", function (event) {
+//       if (event.key == "Enter") {
+//         weather.search();
+//       }
+//     });
+  
+//   weather.fetchWeather("Denver");
